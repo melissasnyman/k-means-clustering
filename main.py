8c7b05598ad9651f4919e1d3c163147a6fde8530
Melissa Snyman <>
2023-10-22 08:26:09 +0200

    Import data

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
 def main():
-    pass
+    airbnb = pd.read_csv('airbnb.csv')
+    latitude = airbnb['latitude']
+    longitude = airbnb['longitude']
+    latitude = latitude.to_numpy()
+    longitude = longitude.to_numpy()
+    plt.scatter(x=longitude, y=latitude)
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+    plt.show()
 
 
 if __name__ == '__main__':
     main()
-    
+
+
